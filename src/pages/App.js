c3c62e13eee5f6c77f1d3a096d669e05a27fc978
Joao Martins <milanezijoao51@gmail.com>
2024-08-15 18:05:55 -0300

    commit parcial

---
+++ src/pages/App.js
@@ -0,0 +1,12 @@
+import Background from '../Assets/githubgenericname.png'
+import {Container} from './styles'
+
+function App() {
+    return (
+    <Container>
+        <img src= {Background} width={72} height={72} alt='github logo'/>
+    </Container>
+  );
+}
+
+export default App;
