4ac3df3387e82d3c8bb01cbc5dd5d6c20afd19d6
Joao Martins <milanezijoao51@gmail.com>
2024-08-16 12:00:25 -0300

    cria componente button

---
+++ src/components/Button/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {ButtonContainer} from './styles'
+
+const Button = () => {
+  return (
+    <ButtonContainer>
+      Buscar
+    </ButtonContainer>
+  )
+}
+
+export default Button
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,14 +1,19 @@
+import { useState } from 'react';
 import Background from '../Assets/githubgenericname.png'
 import Input from '../components/Input'
+import Button from '../components/Button'
 import ItemRepo from '../components/ItemRepo';
 
 import {Container} from './styles'
 
 function App() {
+
+  const [repos,setRepos] = useState([])
     return (
     <Container>
         <img src= {Background} width={72} height={72} alt='github logo'/>
         <Input/>
+        <Button/>
         <ItemRepo/>
     </Container>
   );
