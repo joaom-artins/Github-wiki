000950cc0e5945b54713648a5d9f07e574bd422d
Joao Martins <milanezijoao51@gmail.com>
2024-08-16 10:45:30 -0300

    cria input

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,10 +1,13 @@
 import Background from '../Assets/githubgenericname.png'
+import Input from '../components/Input'
+
 import {Container} from './styles'
 
 function App() {
     return (
     <Container>
         <img src= {Background} width={72} height={72} alt='github logo'/>
+        <Input/>
     </Container>
   );
 }
